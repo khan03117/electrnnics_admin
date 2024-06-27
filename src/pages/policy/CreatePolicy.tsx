80bf47274dc3474bfa1b90ea0bb1a5a5b79c7d16
frantic03117 <shahrukh@franticpro.com>
2024-06-27 19:06:59 +0530

    main

---
+++ src/pages/policy/CreatePolicy.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CreatePolicy = () => {
+    return (
+        <>
+            <section className="py-10">
+                <div className="container">
+
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default CreatePolicy
